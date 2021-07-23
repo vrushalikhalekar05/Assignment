dd7061a5b824b482d50f1ec464be106a98267164
vrushalikhalekar05 <37242893+vrushalikhalekar05@users.noreply.github.com>
2021-07-23 23:38:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -13,44 +13,35 @@ document.getElementById("phno").addEventListener("keyup", myFunction1);
 var ans1,ans2,ans3,answer1,answer2,answer3,phoneNo;
 
 function checkphonef3(c1,c2,c3){
-  var RE=/^[0-9]$/; 
-  if(c1.match(RE) && c2.match(RE) && c3.match(RE))
-  {
+    demo1=document.getElementById("demo1")
+  
     first3=parseInt(c1+c2+c3);
     if(first3>=621 && first3<=799){
     demo1.textContent="reliance";
     console.log("reliance")
-    ans1=true;
+    return true;
     }
     if(first3>=801 && first3<=920){
       console.log("ide")      
       demo1.textContent="idea";
-      ans1=true;
+      return true;
     }
     if(first3>=921 && first3<=999){
       console.log("ide")
       demo1.textContent="vodafone";
-      ans1=true;
+      return true;
     }
     
-  }
-  else
-  {
-    demo1.textContent=""
-    ans1=false
-    
-  }
-  return ans1;
+   return false;
 }
 
 function checkphones3(c4,c5,c6){
   demo2=document.getElementById("demo2")
-
   statesno=[567,456,234,987]
   states=["maharashtra","kerala","delhi","UP"]
-  var RE=/^[0-9]$/; 
-  if(c4.match(RE) && c5.match(RE) && c6.match(RE))
-  {
+   
+    console.log("v2")
+    
     sec3=parseInt(c4+c5+c6);
     if(statesno.includes(sec3)){
       index=statesno.indexOf(sec3)
@@ -58,56 +49,77 @@ function checkphones3(c4,c5,c6){
     demo2.textContent=states[index];
     
     console.log("reliance")
-    ans2=true;
+    return true;
     }
     else{
     demo2.textContent=""
-    ans2=false
+    return false;
     
   }
-  }
-  else{
-    demo2.textContent=""
-    ans2=false
-    
-  }
-  return ans2;
+  
 }
-function checkphonel4(c7,c8,c9,c10){
-  var RE=/^[0-9]$/; 
 
-  if(c7.match(RE) && c8.match(RE) && c9.match(RE) && c10.match(RE))
-  {
-   
-    ans3=true;
-  }
-  else{
-    ans3=false;
-    error2.textContent = "Please enter valid phone no"
-  }
-  return ans3;
-}
 function myFunction1() {
-var demo2 = document.getElementById("demo2");
+      var RE=/^[0-9]$/; 
+      var demo2 = document.getElementById("demo2");
     
       phoneNo = document.getElementById("phno").value;
+      
   
       c1=phoneNo.charAt(0);
   
       c2=phoneNo.charAt(1);
       console.log("c2",c2)
       c3=phoneNo.charAt(2);
-      c4=phoneNo.charAt(3);
-      c5=phoneNo.charAt(4);
-      c6=phoneNo.charAt(5);
-      c7=phoneNo.charAt(6);
-      c8=phoneNo.charAt(7);
-      c9=phoneNo.charAt(8);
-      c10=phoneNo.charAt(9);
-
-      answer1=checkphonef3(c1,c2,c3)
+      if(c1.match(RE) && c2.match(RE) && c3.match(RE))
+      {
+        document.getElementById("phno").value = "("+ phoneNo +") - ";
+        phoneNo = document.getElementById("phno").value;
+        ch1=phoneNo.charAt(1);
+  
+      ch2=phoneNo.charAt(2);
+    
+      ch3=phoneNo.charAt(3);
+        answer1=checkphonef3(ch1,ch2,ch3)
+      }
+      
+      
+      phoneNo = document.getElementById("phno").value;
+      console.log("phoneNo",phoneNo)
+      c4=phoneNo.charAt(8);
+      c5=phoneNo.charAt(9);
+      c6=phoneNo.charAt(10);
+      if(c4.match(RE) && c5.match(RE) && c6.match(RE))
+      {
+        console.log("in 2 ifphoneNo",phoneNo)
+        
+        phoneNo = document.getElementById("phno").value;
+        console.log("in 2 ifphoneNo",phoneNo)
+      
       answer2=checkphones3(c4,c5,c6)
-      answer3=checkphonel4(c7,c8,c9,c10)
+      }
+      console.log("v3")
+      phoneNo = document.getElementById("phno").value;
+      c7=phoneNo.charAt(11);
+      c8=phoneNo.charAt(12);
+      c9=phoneNo.charAt(13);
+      c10=phoneNo.charAt(14);
+      
+      if(c7.match(RE) && c8.match(RE) && c9.match(RE) && c10.match(RE))
+    {  
+      phoneNo = document.getElementById("phno").value;
+
+      phoneNo1 =" - "+ phoneNo.slice(10, 14);
+      document.getElementById("phno").value=phoneNo.slice(0,11)+phoneNo1
+      phoneNo=document.getElementById("phno").value;
+      answer3= true;
+    }
+  else{
+    answer3 = false;
+   
+    }
+      
+      console.log("v5")
       if(answer1 && answer2 && answer3){
          error2.textContent ="correct"
         }
@@ -137,7 +149,6 @@ function emailValidation(){
 function fnameValidation(){
   var fname=  document.getElementById("fname").value;
   splitString1=fname.trim();
-  // splitString1=fname.replace(/\s/g," ")
   splitString1=splitString1.replace(/\s+/g,' ');
 
   splitString=splitString1.split(" ");
@@ -166,20 +177,12 @@ function fnameValidation(){
 }
 function allLetter()
   {
-    // var output = document.getElementById("ip");
-    // var output1 = document.getElementById("myinput");
-   
+    
     mailvalidation = emailValidation();
     nameValidation = fnameValidation();
     if((mailvalidation) && (nameValidation) && (answer1) && (answer2) && (answer3)){
      console.log("in other if");
-      // formfields.style.display="none";
-      // output.style.display="block";
-      
       console.log("splitString1",splitString1)
-      // document.myform.myinput.value = '1';
-      // document.getElementById("demo").innerHTML = "Dear" + splitString[0] + "Thank you for your inquiry.";
-      // output1.value='success';
       var otp = Math.floor(1000 + Math.random() * 9000);
       console.log("random no",otp);
 	
@@ -188,5 +191,4 @@ function allLetter()
 
 	
   }//end of 
-
 
